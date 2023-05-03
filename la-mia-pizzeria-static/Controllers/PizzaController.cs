a1b3f26867a3eeb7882ffee4c4e89ade0b783f88
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-05-03 17:30:52 +0200

    Aggiunto il DB

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -3,29 +3,34 @@
 namespace la_mia_pizzeria_static.Controllers
 {
     public class PizzaController : Controller
-    {
-        List<Pizza> pizze = new List<Pizza>()
-            {
-                new Pizza(0,"Margherita con bufala", "Pizza classica con bufala 100% italiana", 4.00M, "https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg"),
-                new Pizza(1,"Prosciutto e funghi", "Pizza margherita con prosciutto e funghi", 6.00M, "https://assets.afcdn.com/recipe/20210514/120317_w640h486c1cx1060cy707cxb2121cyb1414.webp"),
-                new Pizza(2,"Pizza margherita semplice", "Pizza con margherita classica", 3.50M, "https://images.ctfassets.net/nw5k25xfqsik/64VwvKFqxMWQORE10Tn8pY/200c0538099dc4d1cf62fd07ce59c2af/20220211142754-margherita-9920.jpg?w=1024"),
-           };
+    { 
+
     public IActionResult Index()
         {
-            if (pizze.Count == 0)
-                return View("Error", "Non ci sono pizze!");
+            using(PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = db.Pizza.OrderBy(pizza => pizza.Id).ToList<Pizza>();
 
-            return View(pizze);
+                if (pizze.Count == 0)
+                    return View("Error", "Non ci sono pizze!");
+
+                return View(pizze);
+            }
+
         }
 
-        public IActionResult Details(long id)
-        {
-            Pizza pizza = pizze.FirstOrDefault(p => p.Id == id);
+    public IActionResult Details(long id)
+    {
+            using(PizzaContext db = new PizzaContext())
+            {
+                Pizza pizza = db.Pizza.Where(pizza => pizza.Id == id).FirstOrDefault();
 
-            if (pizza == null)
-                return View("Error", "Nessuna pizza trovata con questo ID!");
+                if (pizza == null)
+                    return View("Error", "Nessuna pizza trovata con questo ID!");
 
-            return View(pizza);
-        } 
+                return View(pizza);
+            }
+
+        }
     }
 }
--- la-mia-pizzeria-static/Pizza.cs
+++ la-mia-pizzeria-static/Pizza.cs
@@ -1,20 +1,17 @@
-﻿namespace la_mia_pizzeria_static
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static
 {
+    [Table("pizza")]
     public class Pizza
     {
+        [Key]
         public long Id { get; set; }
         public string Nome { get; set; }
         public string Descrizione { get; set; }
         public decimal Prezzo { get; set; }
         public string Img { get; set; }
 
-        public Pizza(long id, string nome, string descrizione, decimal prezzo, string img)
-        {
-            Id = id;
-            Nome = nome;
-            Descrizione = descrizione;
-            Prezzo = prezzo;
-            Img = img;
-        }
     }
 }
